c9bdefb3fc77d3f0c2715e892ac67f1b4e29ac5f
Noire <thaitoanvn1349@gmail.com>
2022-07-21 13:13:14 +0700

    ok

--- a.py
+++ a.py
@@ -12,6 +12,7 @@ if i == 0:
                                         for j in range(2):
                                             for j in range(2):
                                                 for j in range(2):
+
                                                     for j in range(2):
                                                         for j in range(2):
                                                             i = 1
